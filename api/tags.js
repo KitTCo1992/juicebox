1848b09979bd64ba4177105378c1c52a99465552
Cody Kitchel <kittco1992@gmail.com>
2021-03-08 21:49:59 -0800

    Created CRUD endpoints

--- api/tags.js
+++ api/tags.js
@@ -7,7 +7,7 @@ tagsRouter.use((req, res, next) => {
   next();
 });
 
-const { getAllTags } = require("../db");
+const { getAllTags, getPostsByTagName } = require("../db");
 
 tagsRouter.get("/", async (req, res) => {
     const tags = await getAllTags();
@@ -15,6 +15,19 @@ tagsRouter.get("/", async (req, res) => {
     res.send({
         tags,
     });
+});
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+  let tagName = req.params.tagName;
+  try {
+    // use our method to get posts by tag name from the db
+    // send out an object to the client { posts: // the posts }
+    let posts = getPostsByTagName(tagName)
+    res.send({ posts });
+  } catch ({ name, message }) {
+    // forward the name and message to the error handler
+    next({ name, message })
+  }
 });
 
 module.exports = tagsRouter;
